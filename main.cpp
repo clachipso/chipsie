9da8e523e1de63d06e38a5727e825b11ef34428c
clachipso <etherstar@gmail.com>
2020-05-24 02:45:22 -0400

    Real initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main(const int argc, const char **argv)
+{
+    cout << "Chipsy the Twitch Chat Bot Starting Up..." << endl;
+
+    cout << "Chipsy the Twitch Chat Bot Shutting Down...Bye Bye!" << endl;
+    return 0;
+}
